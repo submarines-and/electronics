fdb9c4286e72b13b569a42af8e55069035c4d7de
submarines-and <cached_lifters2j@icloud.com>
2026-03-18 16:07:46 +0100

    upload file working somewhat

--- photo-frame/src/gfx.h
+++ photo-frame/src/gfx.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GxEPD2_3C.h>
+#include <SPIFFS.h>
 
 //  Pins for e-ink display
 #define EPD_CS 15
@@ -122,5 +123,84 @@ void renderMessage(const String& msg, const GFXfont* font, int16_t borderMargin)
     do {
         display.fillScreen(GxEPD_WHITE);
         drawCenterLine(display.height() / 2, msg, font, GxEPD_BLACK, borderMargin);
+    } while (display.nextPage());
+}
+
+uint16_t read16(File& f)
+{
+    uint16_t result;
+    f.read((uint8_t*)&result, sizeof(result));
+    return result;
+}
+uint32_t read32(File& f)
+{
+    uint32_t result;
+    f.read((uint8_t*)&result, sizeof(result));
+    return result;
+}
+
+void drawFromFile(const char* filename)
+{
+    display.setFullWindow();
+    display.firstPage();
+    do {
+        File file = SPIFFS.open(filename, "r");
+        if (!file) {
+            Serial.println("File does not exist");
+            return;
+        }
+
+        // 1. Skip BMP Header (14 bytes) and DIB Header (at least 40 bytes)
+        if (read16(file) != 0x4D42) {
+            Serial.println("Not a bmp file");
+            file.close();
+            return;
+        }
+
+        file.seek(10);
+        uint32_t offset = read32(file); // Start of pixel data
+        file.seek(18);
+        uint32_t width = read32(file);
+        int32_t height = (int32_t)read32(file);
+        file.seek(28);
+        uint16_t depth = read16(file); // Bits per pixel (usually 1 or 24)
+
+        // 2. Setup rendering variables
+        bool flip = (height > 0); // BMPs are usually stored bottom-to-top
+        uint32_t absHeight = abs(height);
+        uint32_t rowSize = (width * depth / 8 + 3) & ~3; // Rows are 4-byte padded
+
+        // 3. Iterate through every pixel
+        for (uint32_t i = 0; i < absHeight; i++) {
+            uint32_t row = flip ? (absHeight - 1 - i) : i;
+            file.seek(offset + (row * rowSize));
+
+            for (uint32_t j = 0; j < width; j++) {
+                uint16_t color = GxEPD_WHITE;
+
+                if (depth == 24) {
+                    uint8_t b = file.read();
+                    uint8_t g = file.read();
+                    uint8_t r = file.read();
+                    // Simple thresholding for 3-color (Red/Black/White)
+                    if (r > 200 && g < 100 && b < 100)
+                        color = GxEPD_RED;
+                    else if (r < 128 && g < 128 && b < 128)
+                        color = GxEPD_BLACK;
+                }
+                else if (depth == 1) {
+                    // Read 1-bit monochrome data
+                    static uint8_t b;
+                    if (j % 8 == 0)
+                        b = file.read();
+                    color = (b & (0x80 >> (j % 8))) ? GxEPD_WHITE : GxEPD_BLACK;
+                }
+
+                // GxEPD2 paged loop only renders pixels in the current page buffer
+                display.drawPixel(j, i, color);
+            }
+        }
+
+        file.close();
     } while (display.nextPage());
 }
--- photo-frame/src/main.cpp
+++ photo-frame/src/main.cpp
@@ -45,33 +45,40 @@ void setup()
 
     // curl -v -F "data=@/Users/submarines/Downloads/x.bmp" http://192.168.176.177/upload
     server.on("/upload", HTTP_POST, [](AsyncWebServerRequest* request) { request->send(200, "text/plain", "OK"); }, [](AsyncWebServerRequest* request, String filename, size_t index, uint8_t* data, size_t len, bool final) {
-                  Serial.printf("Upload[%s]: start=%u, len=%u, final=%d\n", filename.c_str(), index, len, final);
+        Serial.printf("Upload[%s]: start=%u, len=%u, final=%d\n", filename.c_str(), index, len, final);
 
         // open the file on first call and store the file handle in the request object
-                  if (!index) {
+        if (!index) {
             SPIFFS.remove("tmp.bmp");
             request->_tempFile = SPIFFS.open("/tmp.bmp", "w");
-                  }
+        }
 
         // stream the incoming chunk to the opened file
-                  if (len) {
+        if (len) {
             request->_tempFile.write(data, len);
-                  }
+        }
 
-                  if (final) {
-                      Serial.printf("UploadEnd: %s, %u B\n", filename.c_str(), index + len);
+        if (final) {
+            Serial.printf("UploadEnd: %s, %u B\n", filename.c_str(), index + len);
             request->_tempFile.close();
 
-                      // flag for later procesing of new image
+            // flag for later procesing of new image
             if (xQueueSend(longOperationQueue, (void*)&filename, (TickType_t)10) != pdPASS) {
-                          request->send(500);
-                      }
-                      else {
-                          request->send(200, "text/plain", "OK");
-                      }
-                  }
-                  //
-              });
+                request->send(500);
+            }
+            else {
+                request->send(200, "text/plain", "OK");
+            }
+        }
+        //
+    });
+
+    /*
+    // Debug echo file
+    server.on("/download", HTTP_GET, [](AsyncWebServerRequest* request) {
+        request->send(SPIFFS, "/tmp.bmp", "img/bmp");
+    });
+    */
 
     server.begin();
 
@@ -91,26 +98,7 @@ void loop()
         String* filename;
         if (xQueueReceive(longOperationQueue, &(filename), (TickType_t)10)) {
             Serial.println("Processing entry in image queue...");
-
-            File file = SPIFFS.open("/x.bmp", "r");
-            size_t size = file.size();
-            uint8_t* buffer = (uint8_t*)malloc(size);
-
-            if (buffer) {
-                file.read(buffer, size);
-                Serial.printf("Read %u bytes successfully\n", size);
-
-            do {
-                display.fillScreen(GxEPD_BLACK);
-                    display.drawBitmap(0, 0, buffer, 528, 880, GxEPD_WHITE);
-            } while (display.nextPage());
-
-                free(buffer);
-                Serial.println("Done!");
-            }
-            else {
-                Serial.println("Failed to allocate memory for buffer");
-            }
+            drawFromFile("/tmp.bmp");
         }
     }
 }
